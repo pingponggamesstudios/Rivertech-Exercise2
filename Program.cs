194ef8e07ca6088aa84f85c24935d2b704dfe79d
Alistair Azzopardi <alistair1695@gmail.com>
2022-08-26 13:00:41 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rivertech___Exercise_2_UI_Automation_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
